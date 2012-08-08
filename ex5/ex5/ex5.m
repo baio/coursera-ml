cc351ae1b3b63012b534e22d4195912f39749615
Max Putilov <max.putilov@gmail.com>
2012-08-08 15:02:12 +0400

    ex5

--- ex5/ex5/ex5.m
+++ ex5/ex5/ex5.m
@@ -126,7 +126,7 @@ end
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-return;
+
 
 %% =========== Part 6: Feature Mapping for Polynomial Regression =============
 %  One solution to this is to use polynomial regression. You should now
@@ -158,8 +158,6 @@ fprintf('  %f  \n', X_poly(1, :));
 fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
-
-
 %% =========== Part 7: Learning Curve for Polynomial Regression =============
 %  Now, you will get to experiment with polynomial regression with multiple
 %  values of lambda. The code below runs polynomial regression with 
@@ -167,7 +165,7 @@ pause;
 %  lambda to see how the fit and learning curve change.
 %
 
-lambda = 0;
+lambda = 1;
 [theta] = trainLinearReg(X_poly, y, lambda);
 
 % Plot training data and fit
